41e429ec40e66f5a5e7297773856688a82127901
Anucha3666 <Anucha.ram3666@gmail.com>
2025-05-09 08:13:19 +0700

    Enhance homepage and navbar with new layout and smooth scrolling functionality

--- src/components/layouts/navbar.tsx
+++ src/components/layouts/navbar.tsx
@@ -1,40 +1,83 @@
+"use client";
+
+import { Button } from "antd";
 import Link from "next/link";
 
 export const Navbar = () => {
   return (
-    <header className='sticky w-full top-0 z-50 border-b bg-background/95 px-2 backdrop-blur supports-[backdrop-filter]:bg-background/60'>
-      <div className='w-full flex py-4 items-center justify-between'>
-        <Link href='/' className='font-bold text-xl'>
+    <header className='sticky w-full h-[3rem] justify-center flex items-center top-0 z-50   shadow-xl bg-background/95 px-2 backdrop-blur supports-[backdrop-filter]:bg-background/60'>
+      <div className='w-full items-center grid  grid-cols-3'>
+        <p
+          className='font-bold text-xl cursor-pointer'
+          onClick={(e) => {
+            e.preventDefault();
+            document
+              .getElementById("home")
+              ?.scrollIntoView({ behavior: "smooth", block: "end" });
+          }}>
           Anucha Rampeungnit
-        </Link>
-        <nav className='hidden md:flex gap-6'>
+        </p>
+        <nav className='hidden md:flex gap-6 justify-center items-center'>
           <Link
             href='#about'
-            className='text-sm font-medium hover:underline underline-offset-4'>
+            className='text-sm font-medium hover:underline underline-offset-4'
+            onClick={(e) => {
+              e.preventDefault();
+              document
+                .getElementById("about")
+                ?.scrollIntoView({ behavior: "smooth", block: "end" });
+            }}>
             About
           </Link>
           <Link
             href='#projects'
-            className='text-sm font-medium hover:underline underline-offset-4'>
+            className='text-sm font-medium hover:underline underline-offset-4'
+            onClick={(e) => {
+              e.preventDefault();
+              document
+                .getElementById("projects")
+                ?.scrollIntoView({ behavior: "smooth", block: "end" });
+            }}>
             Projects
           </Link>
           <Link
             href='#experience'
-            className='text-sm font-medium hover:underline underline-offset-4'>
+            className='text-sm font-medium hover:underline underline-offset-4'
+            onClick={(e) => {
+              e.preventDefault();
+              document
+                .getElementById("experience")
+                ?.scrollIntoView({ behavior: "smooth", block: "end" });
+            }}>
             Experience
           </Link>
           <Link
             href='#certifications'
-            className='text-sm font-medium hover:underline underline-offset-4'>
+            className='text-sm font-medium hover:underline underline-offset-4'
+            onClick={(e) => {
+              e.preventDefault();
+              document
+                .getElementById("certifications")
+                ?.scrollIntoView({ behavior: "smooth", block: "end" });
+            }}>
             Certifications
           </Link>
           <Link
             href='#contact'
-            className='text-sm font-medium hover:underline underline-offset-4'>
+            className='text-sm font-medium hover:underline underline-offset-4'
+            onClick={(e) => {
+              e.preventDefault();
+              document
+                .getElementById("contact")
+                ?.scrollIntoView({ behavior: "smooth", block: "end" });
+            }}>
             Contact
           </Link>
         </nav>
-        <button className=' w-max md:flex'>Download Resume</button>
+        <div className=' flex gap-2 justify-end'>
+          <Button>Resume</Button>
+          <Button>Cv</Button>
+        </div>
       </div>
     </header>
   );
