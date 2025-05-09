974ed34f36564caea9ee869e3d6ba393e6b5804a
Anucha3666 <Anucha.ram3666@gmail.com>
2025-05-09 11:08:13 +0700

    Change the framework from ant design to aceternity ui

--- src/components/home/section-home.tsx
+++ src/components/home/section-home.tsx
@@ -1,16 +1,60 @@
-import { Button, Image } from "antd";
+import {
+  DraggableCardBody,
+  DraggableCardContainer,
+} from "@/components/ui/draggable-card";
+import Image from "next/image";
 import Link from "next/link";
 
 export const SectionHome = () => {
+  const items = [
+    {
+      title: "Sichuan, China 2019",
+      image: "/images/me/sichuan_2019.png",
+      className: "absolute top-50 left-[20%] rotate-[-7deg]",
+    },
+    {
+      title: "Chanthaburi, Thailand 2024",
+      image: "/images/me/chanthaburi_2024.png",
+      className: "absolute top-30 left-[5%] rotate-[-5deg]",
+    },
+    {
+      title: "Rayong, Thailand 2024",
+      image: "/images/me/rayong_2025.png",
+      className: "absolute top-15 left-[25%] rotate-[8deg]",
+    },
+    {
+      title: "Surat Thani, Thailand 2024",
+      image: "/images/me/surat-thani_2024.png",
+      className: "absolute top-50 left-[40%] rotate-[10deg]",
+    },
+    {
+      title: "Kunming, China 2024",
+      image: "/images/me/kunming_2024.png",
+      className: "absolute top-60 left-[10%] rotate-[2deg]",
+    },
+    {
+      title: "Rayong, Thailand 2025",
+      image: "/images/me/rayong_2025.png",
+      className: "absolute top-70 left-[30%] rotate-[-7deg]",
+    },
+    {
+      title: "Mahingsa",
+      image: "/images/me/mahingsa.jpeg",
+      className: "absolute top-42 left-[20%] rotate-[4deg]",
+    },
+  ];
   return (
     <section
       id='home'
       className='flex w-full gap-20 justify-center items-center px-4 md:px-6'
       style={{ height: "calc(100dvh - 3rem)" }}>
-      <div className='flex w-max flex-col justify-center space-y-4'>
+      <div className='flex w-full flex-col justify-center space-y-4'>
         <div className='space-y-2'>
-          <h1 className='text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none'>
-            {"Hi, I'm Anucha Rampeungnit"}
+          <h1 className='text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none text-nowrap'>
+            {"Hi, Guys"}
+          </h1>
+          <h1 className='text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none text-nowrap'>
+            {"I'm Anucha Rampeungnit"}
           </h1>
           <p className='max-w-[600px] text-muted-foreground md:text-xl'>
             Full-Stack Developer & UX Designer with 5+ years of experience
@@ -18,8 +62,12 @@ export const SectionHome = () => {
           </p>
         </div>
         <div className='flex flex-col gap-2 min-[400px]:flex-row'>
-          <Button>Contact Me</Button>
-          <Button>View Projects</Button>
+          <button className='px-4 py-2 rounded-md border border-black bg-white text-black text-sm hover:shadow-[4px_4px_0px_0px_rgba(0,0,0)] transition duration-200'>
+            Contact Me
+          </button>
+          <button className='px-4 py-2 rounded-md border border-black bg-white text-black text-sm hover:shadow-[4px_4px_0px_0px_rgba(0,0,0)] transition duration-200'>
+            View Projects
+          </button>
         </div>
         <div className='flex items-center gap-4 text-sm'>
           <Link
@@ -39,16 +87,29 @@ export const SectionHome = () => {
           </Link>
         </div>
       </div>
-      <div className='flex w-max items-center justify-center'>
-        <div className='relative aspect-square overflow-hidden rounded-full border'>
-          <Image
-            src='/placeholder.svg?height=600&width=600'
-            width={400}
-            height={400}
-            alt='Profile'
-            className='object-cover w-[18rem] h-[18rem]'
-          />
-        </div>
+      <div className='flex w-full h-full items-center justify-center'>
+        <DraggableCardContainer className='relative flex h-full w-full items-center justify-center overflow-clip '>
+          <p className='absolute top-1/2 mx-auto max-w-sm -translate-y-3/4 text-center text-2xl font-black text-neutral-400 md:text-4xl dark:text-neutral-800'>
+            I was born on April 4, 2001.
+          </p>
+          {items?.map((item, i) => (
+            <DraggableCardBody
+              key={`${item?.title}-${i + 1}`}
+              className={item.className}>
+              <Image
+                src={item.image}
+                alt={item.title}
+                width={20}
+                height={20}
+                className='pointer-events-none relative z-10 h-80 w-80 object-cover'
+              />
+
+              <h3 className='mt-4 text-center text-xl font-bold text-neutral-700 dark:text-neutral-300'>
+                {item.title}
+              </h3>
+            </DraggableCardBody>
+          ))}
+        </DraggableCardContainer>
       </div>
     </section>
   );
--- src/components/layouts/navbar.tsx
+++ src/components/layouts/navbar.tsx
@@ -1,82 +1,103 @@
 "use client";
 
-import { Button } from "antd";
-import Link from "next/link";
+import {
+  MobileNav,
+  MobileNavHeader,
+  MobileNavMenu,
+  MobileNavToggle,
+  NavbarButton,
+  NavItems,
+} from "@/components/ui/resizable-navbar";
+import { useState } from "react";
 
 export const Navbar = () => {
+  const navItems = [
+    "About",
+    "Projects",
+    "Experience",
+    "Certifications",
+    "Contact",
+  ];
+
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
   return (
     <header className='sticky w-full h-[3rem] justify-center flex items-center top-0 z-50   shadow-xl bg-background/95 px-2 backdrop-blur supports-[backdrop-filter]:bg-background/60'>
-      <div className='w-full items-center grid  grid-cols-3'>
-        <p
-          className='font-bold text-xl cursor-pointer'
+      <div className='w-full items-center grid grid-cols-3'>
+        <a
+          key={`link-home`}
+          href={`#home`}
+          className='relative px-4 py-2 text-neutral-600 dark:text-neutral-300'
           onClick={(e) => {
             e.preventDefault();
             document
               .getElementById("home")
               ?.scrollIntoView({ behavior: "smooth", block: "end" });
           }}>
-          Anucha Rampeungnit
-        </p>
-        <nav className='hidden md:flex gap-6 justify-center items-center'>
-          <Link
-            href='#about'
-            className='text-sm font-medium hover:underline underline-offset-4'
-            onClick={(e) => {
-              e.preventDefault();
-              document
-                .getElementById("about")
-                ?.scrollIntoView({ behavior: "smooth", block: "end" });
-            }}>
-            About
-          </Link>
-          <Link
-            href='#projects'
-            className='text-sm font-medium hover:underline underline-offset-4'
-            onClick={(e) => {
-              e.preventDefault();
-              document
-                .getElementById("projects")
-                ?.scrollIntoView({ behavior: "smooth", block: "end" });
-            }}>
-            Projects
-          </Link>
-          <Link
-            href='#experience'
-            className='text-sm font-medium hover:underline underline-offset-4'
-            onClick={(e) => {
-              e.preventDefault();
-              document
-                .getElementById("experience")
-                ?.scrollIntoView({ behavior: "smooth", block: "end" });
-            }}>
-            Experience
-          </Link>
-          <Link
-            href='#certifications'
-            className='text-sm font-medium hover:underline underline-offset-4'
-            onClick={(e) => {
-              e.preventDefault();
-              document
-                .getElementById("certifications")
-                ?.scrollIntoView({ behavior: "smooth", block: "end" });
-            }}>
-            Certifications
-          </Link>
-          <Link
-            href='#contact'
-            className='text-sm font-medium hover:underline underline-offset-4'
-            onClick={(e) => {
-              e.preventDefault();
-              document
-                .getElementById("contact")
-                ?.scrollIntoView({ behavior: "smooth", block: "end" });
-            }}>
-            Contact
-          </Link>
-        </nav>
+          <span className='relative z-20 font-bold text-xl cursor-pointer'>
+            Anucha Rampeungnit
+          </span>
+        </a>
+
+        <div>
+          <NavItems items={navItems} />
+          <MobileNav>
+            <MobileNavHeader>
+              <MobileNavToggle
+                isOpen={isMobileMenuOpen}
+                onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+              />
+            </MobileNavHeader>
+
+            <MobileNavMenu
+              isOpen={isMobileMenuOpen}
+              onClose={() => setIsMobileMenuOpen(false)}>
+              {navItems?.map((item, idx) => (
+                <a
+                  key={`mobile-link-${idx}`}
+                  href={`#${item?.toLocaleLowerCase()}`}
+                  className='relative text-neutral-600 dark:text-neutral-300'
+                  onClick={(e) => {
+                    setIsMobileMenuOpen(false);
+                    e.preventDefault();
+                    document
+                      .getElementById(item?.toLocaleLowerCase())
+                      ?.scrollIntoView({ behavior: "smooth", block: "end" });
+                  }}>
+                  <span className='block'>{item}</span>
+                </a>
+              ))}
+              <div className='flex w-full flex-col gap-4'>
+                <NavbarButton
+                  onClick={() => setIsMobileMenuOpen(false)}
+                  variant='primary'
+                  className='w-full'>
+                  Login
+                </NavbarButton>
+                <NavbarButton
+                  onClick={() => setIsMobileMenuOpen(false)}
+                  variant='primary'
+                  className='w-full'>
+                  Book a call
+                </NavbarButton>
+              </div>
+            </MobileNavMenu>
+          </MobileNav>
+        </div>
+
         <div className=' flex gap-2 justify-end'>
-          <Button>Resume</Button>
-          <Button>Cv</Button>
+          <button className='relative inline-flex h-min overflow-hidden rounded-md p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50'>
+            <span className='absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]' />
+            <span className='inline-flex h-full w-full cursor-pointer items-center justify-center rounded-md bg-white px-3 py-1 text-sm font-medium backdrop-blur-3xl'>
+              Resume
+            </span>
+          </button>
+          <button className='relative inline-flex h-min overflow-hidden rounded-md p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50'>
+            <span className='absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]' />
+            <span className='inline-flex h-full w-full cursor-pointer items-center justify-center rounded-md bg-white px-3 py-1 text-sm font-medium backdrop-blur-3xl'>
+              C.V.
+            </span>
+          </button>
         </div>
       </div>
     </header>
